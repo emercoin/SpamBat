63f642d97b19d64d0ca773ce9cd4809059c1529d
Oleg Khovayko <khovayko@gmail.com>
2024-08-17 10:16:04 -0400

    Add comment to use 0.8.6

--- WX_redeem_stamp.php
+++ WX_redeem_stamp.php
@@ -13,7 +13,10 @@ if($stamp_tx == 0) {
     echo "Usage:\n\t$argv[0] Stamp_TX\n";
     exit(1);
 }
-
+// ANTTN:
+// sendrawtransaction incorrectly works in emercoin core 0.8.5, and this code can work correctly
+// in 0.8.6 or higher.
+// Contact support@emercoin.com for details
 $txid = EMC_req('sendrawtransaction', array($stamp_tx, 0), "Unable to broadcast stamp transaction");
 echo "Burn coins: Sent TX=$txid\n";
 
